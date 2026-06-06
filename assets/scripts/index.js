9587bd0ad1359760e1aab5546f708b9914b3d995
Waedyth <lsw587@outlook.com.au>
2026-06-06 20:46:01 +1000

    moved all files to surface level

--- assets/scripts/index.js
+++
@@ -1,9 +0,0 @@
-function automaticScroll() {
-   setTimeout(() => {
-      const contnet_wrapper = document.body.querySelector('.wrapper')
-      if (contnet_wrapper) {
-         contnet_wrapper.classList.add('slide');
-      }
-   }, 1000);
-}
-automaticScroll();
